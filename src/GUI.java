2f4b6f44204a4f294977bdc7ec9bf1b7c995257b
peternamkoong <peternamkoong@gmail.com>
2018-03-03 14:43:05 -0700

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -3,7 +3,9 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.LinkedList;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -21,17 +23,11 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 
-
-
 // issue right now is to break out of the loop and redo the create images method
 //easiest way is to make the load image into a method
 //or figure out a way to only make the user click one button at a time, limit the button clicks?
 
-
-
 // still have to figure out how to pass turns back and forth (should just be an external for loop with the amount of players?)
-
-
 
 //if you want user events you need the eventHandler<ActionEvent> at the end of the class
 public class GUI extends Application {
@@ -64,6 +60,8 @@ public class GUI extends Application {
 	private GameConfiguration game;
 	private Player[] players;
 	int numPlayers;
+	private gamePlayGUI method;
+	private FinalScore score;
 	
 
 	/*
@@ -124,7 +122,7 @@ public class GUI extends Application {
 		//ImageView mainmenu = new ImageView("h.png");
 		
 		layout1.getChildren().addAll(playButton, rulesButton, exitButton);
-		scene1 = new Scene(layout1, 500,500);
+		scene1 = new Scene(layout1, 1000,600);
 	
 		
 		// the play button scene, can choose between easy AI or hard AI
@@ -149,7 +147,7 @@ public class GUI extends Application {
 		
 		layout2.getChildren().addAll(playerPVPButton, easyAIButton, hardAIButton, backButton);
 		
-		scene2 = new Scene(layout2, 500,500);
+		scene2 = new Scene(layout2, 1000	,600);
 		//default 1366, 768
 		
 		
@@ -164,7 +162,7 @@ public class GUI extends Application {
 		
 		layout3.getChildren().addAll(backButton);
 		
-		scene3 = new Scene(layout3, 500, 500);
+		scene3 = new Scene(layout3, 1000, 600);
 		
 		
 		
@@ -213,138 +211,124 @@ public class GUI extends Application {
 			
 		layout4.getChildren().addAll(twoPlayerButton, threePlayerButton, fourPlayerButton, backButton);
 		
-		scene4 = new Scene(layout4, 500, 500);
+		scene4 = new Scene(layout4, 1000, 600);
 		
 		
 		StackPane layout5 = new StackPane();
 		
-		
 		ImageView Background = new ImageView("Background.jpg");
-		Background.setFitWidth(1920);
-		Background.setFitHeight(1080);
+		Background.setFitWidth(1000);
+		Background.setFitHeight(600);
 		
 		HBox boxPic = new HBox(15);
 		HBox FaceDown = new HBox(15);
-		
 		boxPic.setAlignment(Pos.BOTTOM_CENTER);
 		FaceDown.setAlignment(Pos.TOP_CENTER);
-		
-	
+		LinkedList<Button> buttons = new LinkedList<Button>();
 			
-		 // ImageView view = null;
-			ImageView view1 = null;
-	
-		    
-		    Hashtable<String, Integer> board = new Hashtable<String, Integer>();
-
-
-			
-			
-			for (int i = 0; i < game.getHand(0).size(); i++) {
-				
-				
-				ImageView view;
-				
-				view = new ImageView(game.getHand(0).get(i) + ".png");
-				
+		//Images Player 1
+		   Hashtable<String, Integer> board = new Hashtable<String, Integer>();
+			for (int i = 0; i< game.getHand(0).size(); i++) {
+				System.out.println(i+") " + game.getHand(0));
+				ImageView view = new ImageView(game.getHand(0).get(i)+".png");
+				view.setFitWidth(75);
+				view.setFitHeight(100);
 				
 				buttonCards = new Button(null, view);
 				buttonCards.setText(game.getHand(0).get(i));
-			
-			   
-			    buttonCards.setOnAction((e-> {
-			    	
-		
-			  
-			    		
-			    		String cardPlayed;
-			    		SimpleBooleanProperty isDisabled = new SimpleBooleanProperty();
-			    		
-			    		isDisabled.setValue(true);
-			    
-		            ((Button)(e.getSource())).setTranslateX(150);
-		            ((Button)(e.getSource())).setTranslateY(-250);
-		            ((Button)(e.getSource())).disableProperty().bind(isDisabled);
-		            ((Button)(e.getSource())).setAlignment(Pos.BASELINE_RIGHT);
-		            
-		            
-		           
-		            
-		            cardPlayed = ((Button)(e.getSource())).getText();
-		        
+				buttonCards.setContentDisplay(ContentDisplay.TOP);
+				buttons.add(buttonCards);
+				boxPic.getChildren().add(buttonCards);
+				buttons.get(i).setOnAction(e-> {
+					String cardPlayed = ((Button)(e.getSource())).getText();
+					Button buttonPlayed = ((Button)(e.getSource()));
+				//	SimpleBooleanProperty isDisabled = new SimpleBooleanProperty();
+		    		//	isDisabled.setValue(true);
+					System.out.println("hi");
+				//	((Button)(e.getSource())).disableProperty().bind(isDisabled);
+					buttons.remove(0);
 		            game.getHand(0).remove(cardPlayed);
-		            game.updateHand(0,1);
-		        
-		        
-		            System.out.println(game.getHand(0));
-		        
-		        
-		            
-		    			if(board.containsKey(cardPlayed)) {
-		    				board.replace(cardPlayed, (board.get(cardPlayed) + 1));
-		    			}
-		    			else {
-		    				board.put(cardPlayed, 1);
+		            game.updateHand(0,0);
+		            if(board.containsKey(cardPlayed)) {
+	    				board.replace(cardPlayed, (board.get(cardPlayed) + 1));
+	    			}
+	    			else {
+	    				board.put(cardPlayed, 1);
+	    			}
+	    		
+	    				System.out.println(board);
+		            boxPic.getChildren().remove(buttonPlayed);
+		            window.setScene(scene6);
+			
+			});
+			}
+			layout5.getChildren().addAll(Background,boxPic);
+			scene5 = new Scene(layout5, 1000, 600);
+			
+			//Player 2
+			
+			StackPane layout6 = new StackPane();
+			ImageView Background1 = new ImageView("Background.jpg");
+			Background1.setFitWidth(1000);
+			Background1.setFitHeight(600);
+			HBox boxPic1 = new HBox(15);
+			HBox FaceDown1 = new HBox(15);
+			boxPic1.setAlignment(Pos.BOTTOM_CENTER);
+			FaceDown1.setAlignment(Pos.TOP_CENTER);
+			LinkedList<Button> buttons1 = new LinkedList<Button>();
+				
+			//Images Player 2
+			    Hashtable<String, Integer> board1 = new Hashtable<String, Integer>();
+
+				for (int i = 0; i< game.getHand(1).size(); i++) {
+					System.out.println(i+") " + game.getHand(1));
+					ImageView view = new ImageView(game.getHand(1).get(i)+".png");
+					view.setFitWidth(75);
+					view.setFitHeight(100);
+					
+					buttonCards = new Button(null, view);
+					buttonCards.setText(game.getHand(1).get(i));
+					buttonCards.setContentDisplay(ContentDisplay.TOP);
+					buttons1.add(buttonCards);
+					boxPic1.getChildren().add(buttonCards);
+					buttons1.get(i).setOnAction(e-> {
+						String cardPlayed = ((Button)(e.getSource())).getText();
+						Button buttonPlayed = ((Button)(e.getSource()));
+					//	SimpleBooleanProperty isDisabled = new SimpleBooleanProperty();
+			    		//	isDisabled.setValue(true);
+						System.out.println("hi");
+					//	((Button)(e.getSource())).disableProperty().bind(isDisabled);
+						buttons1.remove(0);
+			            game.getHand(1).remove(cardPlayed);
+			            game.updateHand(1,1);
+		    				if(board1.containsKey(cardPlayed)) {
+		    					board1.replace(cardPlayed, (board1.get(cardPlayed) + 1));
+		    				}
+		    				else {
+		    					board1.put(cardPlayed, 1);
 		    			}
 		    		
-		    				System.out.println(board);
-		            
-		            
-		           
-		    				view.setFitWidth(50);
-		    				view.setFitHeight(100);
-		            
-		    				
-		    				
-			    }));
+		    				System.out.println(board1);
+			            boxPic1.getChildren().remove(buttonPlayed);
+			            window.setScene(scene5);
 				
-			   
-		            //should be something like game.getHand("NEXT PLAYER IN CLOCKWISE DIRECITON).size()
-		            
-		            
-		            /*
-		            for (int j = 0; i < game.getHand(1).size(); j++) {
-						view1 = new ImageView(game.getHand(0).get(j) + ".png");
-						buttonCards = new Button(null, view);
-						buttonCards.setText(game.getHand(0).get(j));
-						buttonCards.setMaxWidth(100);
-		            }
-		            
-			    		*/
-			   
-			    
+				});
+				}
+				layout6.getChildren().addAll(Background1,boxPic1);
+				scene6 = new Scene(layout6, 1000, 600);
 				
-				view.setFitWidth(100);
-				view.setFitHeight(150);
-				
-
-				buttonCards.setContentDisplay(ContentDisplay.TOP);
-				boxPic.getChildren().addAll(buttonCards);
-
-			}
-		// }
-
-		layout5.getChildren().addAll(Background,boxPic);
-		
-
-		
-		scene5 = new Scene(layout5, 1366, 766);
-
+		StackPane layout7 = new StackPane();
+		new FinalScore(game);
+		/*	SimpleBooleanProperty isDisabled = new SimpleBooleanProperty();
+	    		isDisabled.setValue(true);
+            ((Button)(e.getSource())).setTranslateX(150);
+            ((Button)(e.getSource())).setTranslateY(-250);
+            ((Button)(e.getSource())).disableProperty().bind(isDisabled);
+            ((Button)(e.getSource())).setAlignment(Pos.BASELINE_RIGHT);
+            */
 
 		window.setTitle("Sushi Go");
 		window.setScene(scene1);
 		window.show();
+			}
 	}
-			
-	}
-
-
-			
-			
-	
-	
-	
-	
-
-	
-	
