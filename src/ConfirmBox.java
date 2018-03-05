e2b18a56949c599e1cf7fd27110c2122ff809922
peternamkoong <peternamkoong@gmail.com>
2018-03-04 18:49:27 -0700

    Update ConfirmBox.java

--- src/ConfirmBox.java
+++ src/ConfirmBox.java
@@ -2,7 +2,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -11,14 +13,14 @@ public class ConfirmBox {
 	
 	static Boolean answer;
 	
-	public static boolean display(String title, String message) {
+	public static boolean display() {
 		Stage window = new Stage();
 		
 		window.initModality(Modality.APPLICATION_MODAL);
-		window.setTitle(title);
+		window.setTitle("Confirmation");
 		window.setMinWidth(300);
 		
-		Label label = new Label(message);
+		Label label = new Label("Sure you want to Exit?");
 		
 		Button yes = new Button("Yes");
 		yes.setOnAction(e -> {
@@ -30,11 +32,15 @@ public class ConfirmBox {
 		answer = false;
 		window.close();
 		});
-		
+		StackPane displayPane = new StackPane();
+		ImageView displayBackground = new ImageView("quit.jpg");
+		displayBackground.setFitWidth(300);
+		displayBackground.setFitHeight(200);
 		VBox layout = new VBox(10);
 		layout.setAlignment(Pos.CENTER);
 		layout.getChildren().addAll(label,yes,no);
-		Scene scene = new Scene(layout, 300, 200);
+		displayPane.getChildren().addAll(displayBackground, layout);
+		Scene scene = new Scene(displayPane, 300, 200);
 		window.setScene(scene);
 		window.showAndWait();
 		return answer;
