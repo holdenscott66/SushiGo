0c166cefbab5d19bf82638aa5e81ff11524299cc
mightymosdan <danielangelestiu@gmail.com>
2018-03-08 16:51:49 -0700

    Add files via upload
    quick update

--- src/SuperClassGUI.java
+++ src/SuperClassGUI.java
@@ -41,86 +41,39 @@ public class SuperClassGUI
 	private ImageView newImage;
 	private VBox newVbox;
 	private HBox newHbox;
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	/** This method turns newButton into a button using
-	 * @param text to show what the text will be on the button
-	 * The intended purpose is to use this objects newButton instanced
-	 * variable to use that button and have it saved as part of a scene
-	 * This will work whenever you want to create a button and just want to 
-	 * have text on it
-	 */
 
-	public void make_Button_Text(String text)
+	//method to make a button,returns a button
+	public Button make_Button(String text)
 	{
-		newButton = new Button(text);
-	}
-	/**
-	 * This method uses
-	 * @param text to create a button with that text and
-	 * @param graphic to use that node as an icon for the button
-	 * 
-	 */
-	public void make_Button_Text_And_Icon(String text, Node graphic)
-	{
-		newButton = new Button(text, graphic);
-		
+		Button new_Button = new Button(text);
+		return new_Button;
 	}
 	
-	/**
-	 * Want that sick fucking button you just made dawg??
-	 * Say no more homie, just get your object and be like object.get_Button() 
-	 * and you get that good good button
-	 */
-	public Button get_Button()
+	//method to make a labe, returns a label
+	public Label make_Label(String name_Label)
 	{
-		return newButton;
+		Label new_Label = new Label(name_Label);
+		return new_Label;
 	}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	/**
-	 * You want a label? Well here you go my dude, just call .make_Label(put label string here) and then call the getter
-	 * and you got your mofuckin label
-	 * @param name_Label
-	 */
-	public void make_Label(String name_Label)
+
+	//method to make an image returns an image
+	public ImageView make_Image(String name_Of_Pic)
 	{
-		newLabel = new Label(name_Label);
-	}
-	/**
-	 * This is the getter I was talking bout. object.get_Label()
-	 * @return newLabel so you get what you ask for
-	 */
-	public Label get_Label()
-	{
-		return newLabel;
-	}
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	/**
-	 * So you want to add pictures huh?
-	 * This method object.make_Image(name of picture) should help you out
-	 * What this mofucka does is it takes
-	 * @param name_Of_Pic as a parameter and has 4 string variables that take that name and 
-	 * add the common picture files. I add those bitches into a list and iterate through
-	 * that list making a new image using those parameters.
-	 * This isnt done yet, because it will need some exception handling if it errors
-	 * out, but once this is done, all you need to make a picture is this function and the
-	 * pic name. you dont even need to add the file extension i got you bro.
-	 */
-	public void make_Image(String name_Of_Pic)
-	{
-		ImageView check_Pic = new ImageView("picture_Name");
 		String picture_Name = name_Of_Pic +".png";
-	
-		newImage = check_Pic;
+		ImageView new_Pic = new ImageView(picture_Name);
+		return new_Pic;
 	}
-	
-	/**
-	 * Get method to get data for the newImage
-	 */
-	public ImageView get_New_Image()
+	//method to make a Vbox returns VBox
+	public VBox makeVbox()
 	{
-		return newImage;
+		VBox newVbox = new VBox ();
+		return newVbox;
 	}
-		}
-		
+	//method to make a Hbox returns VBox
+	public HBox makeHbox()
+	{
+		HBox newHbox = new HBox ();
+		return newHbox;
 	}
+	
 }
